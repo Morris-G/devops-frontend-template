8d7bffc8bb4224877e2f37ce971fb0b6ec5827eb
Morris <gzpgdd@icloud.com>
2022-05-27 17:54:59 +0800

    some updates 1. 升级MagicBox至2.4.9 2. 安装cw-husky

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,11 @@
         <ul>
             <li><a href="#">axios</a></li>
             <li><a href="#">babel</a></li>
+            <li>
+                <a href="http://bkui.fed.canway.com/">
+                    <bk-tag theme="info">bk-magic-box</bk-tag>
+                </a>
+            </li>
             <li><a href="#">eslint</a></li>
             <li><a href="#">nprogress</a></li>
             <li><a href="#">sass & sass-loader</a></li>
@@ -29,17 +34,17 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
